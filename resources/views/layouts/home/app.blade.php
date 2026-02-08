b5da05ae71a623d22185c0d8704f5d144579ac29
zayennn <zaayeennn@gmail.com>
2026-02-08 18:55:10 +0700

    update

--- resources/views/layouts/home/app.blade.php
+++ resources/views/layouts/home/app.blade.php
@@ -38,7 +38,7 @@
     @include('components.home.footer')
 
     {{-- js --}}
-    <script src="{{ asset('home/app.js') }}"></script>
+    <script src="{{ asset('home/js/app.js') }}"></script>
 
     {{-- paroller js --}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
