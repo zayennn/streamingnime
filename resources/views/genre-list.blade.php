a63d4a3a695610025476303397f4193ad20caa89
zayennn <zaayeennn@gmail.com>
2026-02-14 01:02:36 +0700

    update

--- resources/views/genre-list.blade.php
+++ resources/views/genre-list.blade.php
@@ -36,14 +36,14 @@
                         <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
                     </svg>
                     <input type="text" class="search-input" id="genreSearch" placeholder="Search genres...">
-                    <button class="search-clear" id="clearSearch">
+                    {{-- <button class="search-clear" id="clearSearch">
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
                             fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                             stroke-linejoin="round">
                             <line x1="18" y1="6" x2="6" y2="18"></line>
                             <line x1="6" y1="6" x2="18" y2="18"></line>
                         </svg>
-                    </button>
+                    </button> --}}
                 </div>
                 <div class="view-options">
                     <span class="view-label">View:</span>
