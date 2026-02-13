77a7d71b482f148388977f523a9aee3e61516ba4
zayennn <zaayeennn@gmail.com>
2026-02-14 01:42:24 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/anime-list', [HomeController::class, 'animeList'])->name('home');
+Route::get('/anime-list', [HomeController::class, 'animeList'])->name('animelist');
 Route::get('/genre', [App\Http\Controllers\HomeController::class, 'genreList'])->name('genre.list');
 Route::get('/genre/{slug}', [App\Http\Controllers\HomeController::class, 'genreDetail'])->name('genre.detail');
