5977f64c3de609de45563be4222753b3e69ffa2d
zayennn <zaayeennn@gmail.com>
2026-02-16 03:35:00 +0700

    update

--- resources/views/components/home/footer.blade.php
+++ resources/views/components/home/footer.blade.php
@@ -71,7 +71,7 @@
                     <h3 class="link-title">Information</h3>
                     <ul class="link-list">
                         <li><a href="/">About Us</a></li>
-                        <li><a href="/contact">Contact</a></li>
+                        <li><a href="/">Contact</a></li>
                         <li><a href="/faq">FAQ</a></li>
                         <li><a href="/privacy">Privacy Policy</a></li>
                         <li><a href="/terms">Terms of Service</a></li>
